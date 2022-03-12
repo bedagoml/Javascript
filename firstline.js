37a4d172fe99cb9ad9b97ecb06e4642cf98fe0af
Bedah Otieno <otienobedah@gmail.com>
2022-03-12 13:17:43 +0300

    comments in javascript

--- firstline.js
+++ firstline.js
@@ -3,3 +3,8 @@ console.log('this is fun')
 //testing using semicolon at the end
 console.log('Hello World');
 console.log('this is fun');
+
+//introducing comments
+console.log('Hello World')
+console.log('this is fun')
+//comments in javascript
