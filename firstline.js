29db6b5fc3a321959b9b9a260db411b04501ae25
Bedah Otieno <otienobedah@gmail.com>
2022-03-16 07:50:27 +0300

    Using getElementById in javascript function

--- firstline.js
+++ firstline.js
@@ -9,3 +9,6 @@ console.log('Hello World')
 console.log('this is fun')
 //comments in javascript
 //running and working
+// writing functions in Javascript
+// usage of getElementById('id')
+getElementById('id')
